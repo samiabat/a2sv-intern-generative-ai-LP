568aa597a7adaeb6011fdea395bfc76137150c9d
samiabat <samuelabatneh20@gmail.com>
2023-05-17 11:32:08 +0300

    feat(backend-samuel): add domain layer

---
+++ TaskManagement.Domain/Checklist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Domain.Common;
+
+namespace TaskManagement.Domain
+{
+    internal class Checklist: BaseDomainEntity
+    {
+        public Task Task { get; set; }
+        public int TaskId { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ TaskManagement.Domain/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Domain.Common;
+
+namespace TaskManagement.Domain
+{
+    internal class Task: BaseDomainEntity
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+    }
+}
